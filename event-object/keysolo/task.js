ba0a6a5f9d3b798ecb7f16d954a8232ad2b75a93
SergeyBildanov <troktrokov@gmail.com>
2023-12-17 23:23:08 +0400

    keysolo change

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -4,12 +4,64 @@ class Game {
     this.wordElement = container.querySelector('.word');
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
+    this.timer = container.querySelector('.time');
+    this.intervalId = null;
 
     this.reset();
-
     this.registerEvents();
   }
 
+  setTime(hours, minutes, seconds){
+    let time = new Date();
+    time.setHours(hours);
+    time.setMinutes(minutes);
+    time.setSeconds(seconds);
+    return time;    
+  }
+  
+  getTime(time){
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    if(hours < 10){
+        hours = `0${hours}`;
+    }
+    if(minutes < 10){
+        minutes = `0${minutes}`; 
+    }
+    if(seconds < 10){
+        seconds = `0${seconds}`; 
+    }
+    this.timer.textContent = "";
+    this.timer.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+countDown(time){
+  time.setSeconds(time.getSeconds()-1);
+  if(time.getSeconds < 0){
+      time.setMinutes(time.getMinutes()-1);
+      if(time.getMinutes < 0){
+          time.setHours(time.getHours()-1);
+      }
+  }
+  return time;
+}
+  startTime(){
+    if(this.intervalId !== null){
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+    let time = this.setTime(0, 0, this.wordElement.textContent.length);
+    this.getTime(time);
+    this.intervalId = setInterval(() => {
+      this.countDown(time);
+      this.getTime(time);
+      if(this.timer.textContent === "00:00:00"){
+        clearInterval(this.intervalId);
+        this.fail();
+      }
+    }, 1000);
+  }
   reset() {
     this.setNewWord();
     this.winsElement.textContent = 0;
@@ -25,6 +77,17 @@ class Game {
       При неправильном вводе символа - this.fail();
       DOM-элемент текущего символа находится в свойстве this.currentSymbol.
      */
+    document.addEventListener("keydown", (event) => {
+      if(event.key === "Shift" || event.key === "Alt"){
+        return;
+      }
+      if(this.currentSymbol.textContent === event.key.toLowerCase() && this.timer.textContent !== "00:00:00"){
+        this.success();
+      }
+      else{
+        this.fail();
+      }
+    }); 
   }
 
   success() {
@@ -56,21 +119,22 @@ class Game {
     const word = this.getWord();
 
     this.renderWord(word);
+    this.startTime();
   }
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
+        'bob крутой парень',
+        'awesome задача',
+        'netology крутое место for studying',
+        'hello всем',
         'kitty',
-        'rock',
+        'synthwave фанатам of ryan gosling',
         'youtube',
         'popcorn',
         'cinema',
         'love',
-        'javascript'
+        'javascript is крутой язык программирования'
       ],
       index = Math.floor(Math.random() * words.length);
 
@@ -89,6 +153,5 @@ class Game {
     this.currentSymbol = this.wordElement.querySelector('.symbol_current');
   }
 }
-
 new Game(document.getElementById('game'))
 
