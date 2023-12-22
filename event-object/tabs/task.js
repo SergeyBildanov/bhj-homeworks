53996e6e5868e789c77d8588085e66ac0cb8d633
SergeyBildanov <troktrokov@gmail.com>
2023-12-22 20:29:45 +0400

    Tabs Corrected

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,14 +1,18 @@
 let tabs = document.querySelectorAll(".tab");
 let contents = document.querySelectorAll(".tab__content");
+let current = document.querySelector(".tab_active");
 let currentContent = document.querySelector(".tab__content_active");
 
 for(let i=0; i<tabs.length; i++){
     tabs[i].addEventListener("click", () => {
-        if(currentContent === contents[i]){
+        if(current === tabs[i]){
             return;
         }
-        contents[i].classList.toggle("tab__content_active");
+        tabs[i].classList.add("tab_active");
+        contents[i].classList.add("tab__content_active")
+        current.classList.remove("tab_active");
+        current = tabs[i];
         currentContent.classList.remove("tab__content_active");
-        currentContent = contents[i];
+        currentContent = contents[i];  
     });
 }
