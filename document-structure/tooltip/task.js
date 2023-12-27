8ebe4ac61acb3f37dd89e2b55844f840d1e6c8a8
SergeyBildanov <troktrokov@gmail.com>
2023-12-28 02:09:51 +0400

    tooltips changed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,5 +1,5 @@
 let tips = document.querySelectorAll(".has-tooltip");
-let positions = ["top", "left", "bottom", "right"];
+let positions = ["bottom", "top", "left", "right"];
 
 document.addEventListener("DOMContentLoaded", () => {
     for(let i=0; i<tips.length; i++){
@@ -15,7 +15,18 @@ let tip_texts = document.getElementsByClassName("tooltip");
 for(let i=0; i<tips.length; i++){
     tips[i].addEventListener("click", e => {
         Array.from(tip_texts)[i].classList.toggle("tooltip_active");
-        Array.from(tip_texts)[i].style = `${Array.from(tip_texts)[i].dataset.position}:0`;
+        if(Array.from(tip_texts)[i].dataset.position === "bottom"){
+            Array.from(tip_texts)[i].style = `left: ${e.currentTarget.getBoundingClientRect().left}px`
+        }
+        if(Array.from(tip_texts)[i].dataset.position === "top"){
+            Array.from(tip_texts)[i].style = `left: ${e.currentTarget.getBoundingClientRect().left}px; top: ${e.currentTarget.getBoundingClientRect().top - 30}px;`
+        }
+        if(Array.from(tip_texts)[i].dataset.position === "left"){
+            Array.from(tip_texts)[i].style = `left: ${e.currentTarget.getBoundingClientRect().left - tip_texts[i].getBoundingClientRect().width}px; top: ${e.currentTarget.getBoundingClientRect().top - 15}px;`
+        }
+        if(Array.from(tip_texts)[i].dataset.position === "right"){
+            Array.from(tip_texts)[i].style = `left: ${e.currentTarget.getBoundingClientRect().left + e.currentTarget.getBoundingClientRect().width}px; top: ${e.currentTarget.getBoundingClientRect().top - 15}px;`
+        }
         if(Array.from(document.getElementsByClassName("tooltip_active")).length > 1){
             current.classList.remove("tooltip_active");
         }
