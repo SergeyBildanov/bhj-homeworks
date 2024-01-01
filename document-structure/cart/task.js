209d9ccb4be9f74d768ffe1304c4c896478bafe3
SergeyBildanov <troktrokov@gmail.com>
2024-01-01 08:53:21 +0400

    cart upgraded

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -6,6 +6,7 @@ let productsImages = document.querySelectorAll(".product img");
 let cart = document.querySelector(".cart__products");
 let cartProducts = document.getElementsByClassName("cart__product");
 let myStorage1 = this.localStorage;
+let productInner;
 
 
 for(let i=0; i<incs.length; i++){
@@ -32,19 +33,24 @@ for(let i=0; i<adds.length; i++){
                     anim.style.position = "absolute";
                     let top = anim.getBoundingClientRect().top;
                     let left = anim.getBoundingClientRect().left;
+                    let targetTop = Array.from(cartProducts)[j].querySelector("img").getBoundingClientRect().top;
+                    let targetHeight = Array.from(cartProducts)[j].querySelector("img").getBoundingClientRect().height;
+                    let targetWidth = Array.from(cartProducts)[j].querySelector("img").getBoundingClientRect().width;
+                    let targetLeft = Array.from(cartProducts)[j].querySelector("img").getBoundingClientRect().left;
                     anim.style.top = `${top}px`;
                     anim.style.left = `${left}px`;
                     let intervalId = setInterval(() => {
-                        top = top - 10;
                         left = left + 10;
+                        top = top - 10*(Array.from(cartProducts)[j].dataset.id)*((targetTop+targetHeight)/(targetLeft + targetWidth));
                         anim.style.top = `${top}px`;
                         anim.style.left = `${left}px`;
-                        if(top < Array.from(cartProducts)[j].getBoundingClientRect().top){
+                        if(left > targetLeft){
                             clearInterval(intervalId);
                             anim.remove()
                         }
                     });
-                    myStorage1.setItem( `products`, document.querySelector(".cart__products").outerHTML);
+                    productInner = getProductInner(cartProducts);
+                    myStorage1.setItem( `products`, JSON.stringify(productInner));
                     return;
                 }
             }
@@ -62,31 +68,30 @@ for(let i=0; i<adds.length; i++){
             +
         </div>
         </div></div>`);
-        myStorage1.setItem( `products`, document.querySelector(".cart__products").outerHTML);
+        productInner = getProductInner(cartProducts);
+        myStorage1.setItem(`products`, JSON.stringify(productInner));
         document.querySelector(".cart").style.display = "block";
         values[i].innerText = 1;
         for(let k=0; k<Array.from(cartProducts).length; k++){
             Array.from(cartProducts)[k].querySelector(".product__quantity-control_dec").onclick = (e) => {
-                for(let j=0; j<cartProducts.length; j++){
-                    if(e.currentTarget.parentElement.closest(".cart__product").dataset.id === cartProducts[j].dataset.id){
-                        let count = Array.from(cartProducts)[j].querySelector(".cart__product-count").innerText - 1;
-                        if(count === 0){
-                            Array.from(cartProducts)[j].remove();
-                            myStorage1.setItem( `products`, document.querySelector(".cart__products").outerHTML);
-                            if(Array.from(cartProducts).length === 0){
-                                document.querySelector(".cart").style.display = "none";
-                            }
-                        }
-                        else{
-                            Array.from(cartProducts)[j].querySelector(".cart__product-count").textContent--;
-                            myStorage1.setItem( `products`, document.querySelector(".cart__products").outerHTML);
-                        }
+                let product = e.currentTarget.parentElement.closest(".cart__product");
+                let count = product.querySelector(".cart__product-count").innerText - 1;
+                if(count === 0){
+                    product.remove();
+                    if(Array.from(cartProducts).length === 0){
+                        document.querySelector(".cart").style.display = "none";
                     }
                 }
+                else{
+                    product.querySelector(".cart__product-count").textContent--;
+                }
+                productInner = getProductInner(cartProducts);
+                myStorage1.setItem( `products`, JSON.stringify(productInner));
             };
             Array.from(cartProducts)[k].querySelector(".product__quantity-control_inc").onclick = () => {
                 Array.from(cartProducts)[k].querySelector(".cart__product-count").textContent++;
-                myStorage1.setItem( `products`, document.querySelector(".cart__products").outerHTML);
+                productInner = getProductInner(cartProducts);
+                myStorage1.setItem( `products`, JSON.stringify(productInner));
             };
         }
 });
@@ -94,23 +99,48 @@ for(let i=0; i<adds.length; i++){
 
 window.onload = () => {
     if(myStorage1.length){
-        cart.outerHTML = myStorage1.getItem("products");
+        let productsInners = JSON.parse(myStorage1.getItem("products"));
+        let cart = document.querySelector(".cart");
+        if(productsInners === ''){
+            cart.innerHTML = cart.innerHTML;
+            return;
+        }
+        let keys = Object.keys(productsInners);
+        for(let i=0; i<keys.length; i++){
+            document.querySelector(".cart__products").insertAdjacentHTML("afterBegin", `
+            <div class="cart__product" data-id="${productsInners[keys[keys.length-i-1]].number}">
+            <img class="cart__product-image" src="${productsInners[keys[keys.length-i-1]].src}" alt="">
+            <div class="cart__product-count">${productsInners[keys[keys.length-i-1]].count}</div>
+            <div>
+            <div class="product__quantity-control product__quantity-control_dec">
+            -
+            </div>
+            <div class="product__quantity-control product__quantity-control_inc">
+            +
+            </div>
+            </div></div>`);
+        }
     }
-    for(let i=0; i<Array.from(cartProducts).length; i++){
-        Array.from(cartProducts)[i].querySelector(".product__quantity-control_dec").onclick = () => {
-            let count = Array.from(cartProducts)[i].querySelector(".cart__product-count").textContent - 1;
+    for(let k=0; k<Array.from(cartProducts).length; k++){
+        Array.from(cartProducts)[k].querySelector(".product__quantity-control_dec").onclick = (e) => {
+            let product = e.currentTarget.parentElement.closest(".cart__product");
+            let count = product.querySelector(".cart__product-count").innerText - 1;
             if(count === 0){
-                Array.from(cartProducts)[i].remove();
-                myStorage1.setItem( `products`, document.querySelector(".cart__products").outerHTML);
+                product.remove();
+                if(Array.from(cartProducts).length === 0){
+                    document.querySelector(".cart").style.display = "none";
+                }
             }
             else{
-                Array.from(cartProducts)[i].querySelector(".cart__product-count").textContent--;
-                myStorage1.setItem( `products`, document.querySelector(".cart__products").outerHTML);
+                product.querySelector(".cart__product-count").textContent--;
             }
+            productInner = getProductInner(cartProducts);
+            myStorage1.setItem( `products`, JSON.stringify(productInner));
         };
-        Array.from(cartProducts)[i].querySelector(".product__quantity-control_inc").onclick = () => {
-            Array.from(cartProducts)[i].querySelector(".cart__product-count").textContent++;
-            myStorage1.setItem( `products`, document.querySelector(".cart__products").outerHTML);
+        Array.from(cartProducts)[k].querySelector(".product__quantity-control_inc").onclick = () => {
+            Array.from(cartProducts)[k].querySelector(".cart__product-count").textContent++;
+            productInner = getProductInner(cartProducts);
+            myStorage1.setItem( `products`, JSON.stringify(productInner));
         };
     }
     if(Array.from(cartProducts).length !== 0){
@@ -119,4 +149,19 @@ window.onload = () => {
     else{
         document.querySelector(".cart").style.display = "none";
     }
+}
+
+
+function getProductInner(arr){
+    debugger
+    let result = [];
+    let product;
+    for(let i=0; i< arr.length; i++){
+        product = {};
+        product.src = arr[i].querySelector("img").src;
+        product.count = arr[i].querySelector(".cart__product-count").textContent;
+        product.number = arr[i].dataset.id;
+        result.push(product);
+    }
+    return result;
 }
