cb19d3730a77ec90b572384f45d8a1c83195992e
SergeyBildanov <troktrokov@gmail.com>
2024-01-09 03:05:21 +0400

    poll difficult init

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -2,6 +2,7 @@ let xhr = new XMLHttpRequest();
 let url = "https://students.netoservices.ru/nestjs-backend/poll";
 let pollTitle = document.getElementById("poll__title");
 let pollAnswers = document.getElementById("poll__answers");
+let poll = document.querySelector(".card");
 
 xhr.open("GET", url);
 xhr.send();
@@ -22,7 +23,25 @@ xhr.addEventListener('readystatechange', () => {
         for(let i=0; i < Array.from(buttons).length; i++){
             Array.from(buttons)[i].addEventListener("click", () => {
                 alert("Спасибо, ваш голос засчитан!");
-                return;
+                const answerXhr = new XMLHttpRequest();
+                let send = `vote=${JSON.parse(xhr.responseText).id}&answer=${i}`;
+                answerXhr.open( 'POST', 'https://students.netoservices.ru/nestjs-backend/poll' );
+                answerXhr.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' );
+                answerXhr.send(send);
+                answerXhr.addEventListener("readystatechange", () => {
+                    if(answerXhr.readyState === answerXhr.DONE){
+                        let voteArray = JSON.parse(answerXhr.responseText).stat;
+                        let sum = 0;
+                        for(let j=0; j<voteArray.length; j++){
+                            sum = sum + voteArray[j].votes;
+                        }
+                        for(let j=0; j<voteArray.length; j++){
+                            let text = `<div>${voteArray[j].answer}: <strong>${((voteArray[j].votes * 100)/(sum)).toFixed(2)}%</strong></div>`;
+                            poll.insertAdjacentHTML("beforeEnd", text);
+                        }
+                        pollAnswers.style.display = "none";
+                    }
+                })
             });
         }
     }
